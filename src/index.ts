b4c4640cfad5e55628eb7d28d9821c17c62c1c27
Foxy <foxy.github@gmail.com>
2025-04-04 16:02:39 +0200

    Added user api

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,16 @@ import { FlightRoute } from './routes/flight.route'
 import { configDotenv } from 'dotenv'
 import { AppDataSource } from './db'
 import { AirlineRoute } from './routes/airline.route'
+import { UserRoute } from './routes/user.route'
 
 const app = express()
+app.use(express.json())
 app.use(cors())
 app.use(morgan('tiny'))
 
 app.use('/api/flight', FlightRoute)
 app.use('/api/airline', AirlineRoute)
+app.use('/api/user', UserRoute)
 
 app.get('*', (req, res) => {
     res.status(404).json({
@@ -22,12 +25,12 @@ app.get('*', (req, res) => {
 
 configDotenv()
 AppDataSource.initialize()
-    .then(() =>{
-        console.log('Connected to dataset')
+    .then(() => {
+        console.log('Connected to database')
         const port = process.env.SERVER_PORT || 3000
         app.listen(port, () => console.log(`Application started on port ${port}`))
     })
-    .catch(e=>{
-        console.log('Databaser server connection failed')
+    .catch(e => {
+        console.log('Database server connection failed')
         console.log(e)
     })
