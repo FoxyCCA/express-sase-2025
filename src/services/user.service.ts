70ac6104b1b6f1c7f3a539b35dd0afc5a5d47de2
Foxy <foxy.github@gmail.com>
2025-04-04 16:23:36 +0200

    Added data authentication and incryption

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,6 +4,7 @@ import { User } from "../entities/User";
 import type { LoginModel } from "../models/login.model";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import type { Response } from "express";
 
 const repo = AppDataSource.getRepository(User)
 const tokenSecret = process.env.JWT_SECRET
@@ -23,7 +24,9 @@ export class UserService {
 
             return {
                 name: user?.email,
+                //@ts-ignore
                 access: jwt.sign(payload, tokenSecret, { expiresIn: accessTTL }),
+                //@ts-ignore
                 refresh: jwt.sign(payload, tokenSecret, { expiresIn: refreshTTL })
             }
         }
@@ -31,6 +34,39 @@ export class UserService {
         throw new Error('BAD_CREDENTIALS')
     }
 
+static async verifyToken(req: any, res: Response, next: Function) {
+        const whitelist = ['/api/user/login']
+
+        if (whitelist.includes(req.path)) {
+            next()
+            return
+        }
+
+        const authHeader = req.headers['authorization']
+        const token = authHeader && authHeader.split(' ')[1]
+
+        if (token == undefined) {
+            res.status(401).json({
+                message: 'NO_TOKEN_FOUND',
+                timestamp: new Date()
+            })
+            return
+        }
+
+        //@ts-ignore
+        jwt.verify(token, tokenSecret, (err:any, user: any) =>{
+            if (err) {
+                res.status(403).json({
+                    message: 'INVALID_TOKEN',
+                    timestamp: new Date()
+                })
+                return
+            }
+
+            req.user = user
+            next()
+        })
+    }
     static async getUserByEmail(email: string) {
         const data = repo.findOne({
             where: {
